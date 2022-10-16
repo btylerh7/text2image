9b22504fe82ade43a2f75eb5868d685fcde5c266
Tyler Baker <tylerhbaker99@gmail.com>
2022-10-15 23:45:43 -0400

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use(express.urlencoded({extended:false}))
+app.use(cors({
+    origin: "*"
+}))
+app.use(express.json())
 app.use('/api', require('./api/v1/routes/converterRoutes'))
 
 app.listen(process.env.PORT || 3000, () => {
